d29879f70f5f3d845bf2d2a9bd2dec819215f1c0
Scorpikk <scorrrpik@gmail.com>
2017-04-22 11:42:04 +0300

    Splash

---
+++ app/src/main/java/com/aiaiai/bestfintechappever/activity/Splash.java
@@ -0,0 +1,15 @@
+package com.aiaiai.bestfintechappever.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.aiaiai.bestfintechappever.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
