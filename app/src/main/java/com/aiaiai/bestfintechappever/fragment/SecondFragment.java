1c796b55547d10da9cce6921298c275377b77518
Scorpikk <scorrrpik@gmail.com>
2017-04-22 21:18:11 +0300

    Second fragment

---
+++ app/src/main/java/com/aiaiai/bestfintechappever/fragment/SecondFragment.java
@@ -0,0 +1,13 @@
+package com.aiaiai.bestfintechappever.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Slavik on 22.04.2017.
+ * Yippie-Kai-Yay!
+ */
+
+public class SecondFragment extends Fragment {
+
+
+}
