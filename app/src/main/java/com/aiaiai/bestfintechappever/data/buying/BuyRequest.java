2d855c61329135c7c11ad7e5f9809c6d22dc5656
Kirlill Makarov <kir-maka@yandex.ru>
2017-04-23 02:44:25 +0300

    fix problem with default id in buying response

--- app/src/main/java/com/aiaiai/bestfintechappever/data/buying/BuyRequest.java
+++ app/src/main/java/com/aiaiai/bestfintechappever/data/buying/BuyRequest.java
@@ -1,10 +1,12 @@
 package com.aiaiai.bestfintechappever.data.buying;
 
 public class BuyRequest {
+    public BuyRequest(long user_id, long biglion_id) {
+        this.user_id = user_id;
+        this.biglion_id = biglion_id;
+    }
+
     long user_id;
     long biglion_id;
 
-    public BuyRequest(long l, long itemId) {
-
-    }
 }
