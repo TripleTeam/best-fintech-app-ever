01a4b2f90ade680f80038d4624d1fa10a2e3bee1
Kirlill Makarov <kir-maka@yandex.ru>
2017-04-23 01:59:32 +0300

    add api methods

--- app/src/main/java/com/aiaiai/bestfintechappever/data/ApiRetrofitService.java
+++ app/src/main/java/com/aiaiai/bestfintechappever/data/ApiRetrofitService.java
@@ -1,12 +1,17 @@
 package com.aiaiai.bestfintechappever.data;
 
+import com.aiaiai.bestfintechappever.data.buying.BuyRequest;
+import com.aiaiai.bestfintechappever.data.history.HistoryItem;
 import com.aiaiai.bestfintechappever.data.offer.NetworkOffer;
 import com.aiaiai.bestfintechappever.model.LetyShopsOffer;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiRetrofitService {
 
@@ -15,4 +20,15 @@ public interface ApiRetrofitService {
 
     @GET("letyshops/offers")
     Call<List<LetyShopsOffer>> getLettyOffers();
+
+    @GET("user/get_cashback")
+    Call<Void> getCashBack(@Query("user_id") long userId);
+
+    @GET("user/get_history")
+    Call<List<HistoryItem>> getHistory(@Query("user_id") long userId);
+
+
+    @POST("biglion/buy")
+    Call<Void> postBuy(@Body BuyRequest buyRequest);
+
 }
