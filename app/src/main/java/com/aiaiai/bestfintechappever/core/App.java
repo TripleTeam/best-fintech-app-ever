ca7500a3b1022d14bdcacd7234716248812d57f1
Kirlill Makarov <kir-maka@yandex.ru>
2017-04-22 15:11:34 +0300

    set portrait orientation for the app

--- app/src/main/java/com/aiaiai/bestfintechappever/core/App.java
+++ app/src/main/java/com/aiaiai/bestfintechappever/core/App.java
@@ -1,8 +1,23 @@
 package com.aiaiai.bestfintechappever.core;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 public class App extends Application {
     //here we can init something for dagger/utils/etc
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //set portrait orientation for app
+        registerActivityLifecycleCallbacks(new ActivityLifecycleStub() {
+            @Override
+            public void onActivityCreated(Activity a, Bundle savedInstanceState) {
+                a.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        });
+    }
 
 }
