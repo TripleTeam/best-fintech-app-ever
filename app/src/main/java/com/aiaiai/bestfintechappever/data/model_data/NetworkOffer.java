9244f7dd032593bf8b44856495714c92267f0163
Kirlill Makarov <kir-maka@yandex.ru>
2017-04-22 18:08:42 +0300

    add network offer instance

---
+++ app/src/main/java/com/aiaiai/bestfintechappever/data/model_data/NetworkOffer.java
@@ -0,0 +1,12 @@
+package com.aiaiai.bestfintechappever.data.model_data;
+
+public class NetworkOffer {
+    String category;
+    String description;
+    String discount;
+    String discounted;
+    String href;
+    String imgSrcUrl;
+    String imgUrl;
+    String price;
+}
