193f13e407ac127ecdb00db77b9e12a1139cd331
Scorpikk <scorrrpik@gmail.com>
2017-04-22 12:15:05 +0300

    Main

---
+++ app/src/main/java/com/aiaiai/bestfintechappever/activity/MainActivity.java
@@ -0,0 +1,47 @@
+package com.aiaiai.bestfintechappever.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.aiaiai.bestfintechappever.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    textMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    textMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    textMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+    }
+
+}
--- app/src/main/java/com/aiaiai/bestfintechappever/activity/Splash.java
+++ app/src/main/java/com/aiaiai/bestfintechappever/activity/Splash.java
@@ -1,5 +1,6 @@
 package com.aiaiai.bestfintechappever.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,5 +12,9 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
     }
 }
