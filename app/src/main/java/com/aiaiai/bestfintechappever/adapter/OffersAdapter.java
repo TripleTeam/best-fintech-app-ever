b51684a5985d97f486614aa0f09b936fef9f55f0
Scorpikk <scorrrpik@gmail.com>
2017-04-22 20:29:15 +0300

    Add letyshops model, vh Update adapter

--- app/src/main/java/com/aiaiai/bestfintechappever/adapter/OffersAdapter.java
+++ app/src/main/java/com/aiaiai/bestfintechappever/adapter/OffersAdapter.java
@@ -7,8 +7,11 @@ import android.view.ViewGroup;
 
 import com.aiaiai.bestfintechappever.R;
 import com.aiaiai.bestfintechappever.model.Offer;
+import com.aiaiai.bestfintechappever.model.OfferLetyShops;
+import com.aiaiai.bestfintechappever.model.vh.OfferLetyShopsVH;
 import com.aiaiai.bestfintechappever.model.vh.OfferVH;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,26 +21,56 @@ import java.util.List;
 
 public class OffersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
+    private final int OFFER_BIGLION = 0;
+    private final int OFFER_LETYSHOPS = 1;
+
     private LayoutInflater inflater;
     private List<Offer> offers;
+    private List<OfferLetyShops> offersLetyShops = new ArrayList<>();
 
     public OffersAdapter(Context context, List<Offer> offers) {
         this.inflater = LayoutInflater.from(context);
         this.offers = offers;
     }
 
+    public OffersAdapter(Context context, List<Offer> offers, List<OfferLetyShops> offersLetyShops) {
+        this(context, offers);
+        this.offersLetyShops = offersLetyShops;
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new OfferVH(inflater.inflate(R.layout.item_offer, parent, false));
+        switch (viewType) {
+            case 0:
+                return new OfferVH(inflater.inflate(R.layout.item_offer_biglion, parent, false));
+            case 1:
+                return new OfferLetyShopsVH(inflater.inflate(R.layout.item_offer_biglion, parent, false));
+        }
+        return null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        ((OfferVH) holder).bind(offers.get(position));
+        switch (getItemViewType(position)) {
+            case 0:
+                ((OfferVH) holder).bind(offers.get(position));
+                break;
+            case 1:
+                ((OfferLetyShopsVH) holder).bind(offersLetyShops.get(position));
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return offers.size();
+        return offers.size() + offersLetyShops.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position < offers.size()) {
+            return 0;
+        }
+        return 1;
     }
 }
