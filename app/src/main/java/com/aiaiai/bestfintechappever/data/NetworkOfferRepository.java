eec911e7803fd9b7d07c5daf480f8eec21350e0b
Kirlill Makarov <kir-maka@yandex.ru>
2017-04-22 18:33:28 +0300

    provide to network repository concurrency classes

--- app/src/main/java/com/aiaiai/bestfintechappever/data/NetworkOfferRepository.java
+++ app/src/main/java/com/aiaiai/bestfintechappever/data/NetworkOfferRepository.java
@@ -1,15 +1,35 @@
 package com.aiaiai.bestfintechappever.data;
 
+import com.aiaiai.bestfintechappever.async.MainHandler;
+
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class NetworkOfferRepository implements OfferRepository {
 
-    NetworkOfferRepository (ThreadPoolExecutor threadPoolExecutor) {
+    private final ThreadPoolExecutor threadPoolExecutor;
+    private final MainHandler mainHandler;
 
+    NetworkOfferRepository(ThreadPoolExecutor threadPoolExecutor, MainHandler mainHandler) {
+
+        this.threadPoolExecutor = threadPoolExecutor;
+        this.mainHandler = mainHandler;
     }
 
     @Override
     public void prepareOffers(Callback callback) {
+        threadPoolExecutor.execute(new Runnable() {
+
+
+            @Override
+            public void run() {
+                mainHandler.post(new MainHandler.OnMainThread() {
+                    @Override
+                    public void doOnMain() {
+
+                    }
+                });
+            }
+        });
 
 
     }
